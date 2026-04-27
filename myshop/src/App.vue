a68f1792528ad53503a98c937c33a90ae914e3a8
code-with-theo <1530442576@qq.com>
2026-04-27 16:43:31 +0800

    Add shop project

---
+++ myshop/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router';
+
+</script>
+
+<template>
+  <!-- 一级路由出口 -->
+  <RouterView></RouterView>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ myshop/src/views/SubCategory/index.vue
@@ -0,0 +1,132 @@
+<script setup>
+import { getCategoryFilterAPI } from '@/apis/category'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { getSubCategoryAPI } from '@/apis/category'
+import GoodsItem from '../Home/components/GoodsItem.vue'
+//二级面包屑导航
+const filterData = ref({})
+const route = useRoute()
+const getFilterData = async () => {
+  const res = await getCategoryFilterAPI(route.params.id)
+  filterData.value = res.result
+}
+onMounted(() => {
+  getFilterData()
+})
+//列表
+const goodList = ref([])
+const reqData = ref({
+  categoryId: route.params.id,
+  page: 1,
+  pageSize: 20,
+  sortField: 'publishTime'
+})
+  
+const getGoodList = async () => {
+  const res = await getSubCategoryAPI(reqData.value)
+  console.log(res)
+  goodList.value = res.result.items
+}
+onMounted(() => getGoodList())
+//Tab切换
+const tabChange = () => {
+  console.log('tab切换了', reqData.value.sortField)
+  reqData.value.page = 1
+  getGoodList()
+}
+
+//无限数据
+const disabled = ref(false)
+const load = async () => {
+  console.log('加载更多数据')
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value, ...res.result.items]
+  if (res.result.items.length === 0) {
+    disabled.value = true
+  }
+}
+</script>
+
+<template>
+  <div class="container ">
+    <!-- 面包屑 -->
+    <div class="bread-container">
+      <el-breadcrumb separator=">">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item :to="{ path: `/category/${filterData.parentId}` }">{{ filterData.parentName }}</el-breadcrumb-item>
+        <el-breadcrumb-item>{{ filterData.name }}</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="sub-container">
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
+        <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
+        <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
+        <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
+      </el-tabs>
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
+         <GoodsItem v-for="item in goodList" :key="item.id" :goods="item" />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.bread-container {
+  padding: 25px 0;
+  color: #666;
+}
+
+.sub-container {
+  padding: 20px 10px;
+  background-color: #fff;
+
+  .body {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 10px;
+  }
+
+  .goods-item {
+    display: block;
+    width: 220px;
+    margin-right: 20px;
+    padding: 20px 30px;
+    text-align: center;
+
+    img {
+      width: 160px;
+      height: 160px;
+    }
+
+    p {
+      padding-top: 10px;
+    }
+
+    .name {
+      font-size: 16px;
+    }
+
+    .desc {
+      color: #999;
+      height: 29px;
+    }
+
+    .price {
+      color: $priceColor;
+      font-size: 20px;
+    }
+  }
+
+  .pagination-container {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+  }
+
+
+}
+</style>
